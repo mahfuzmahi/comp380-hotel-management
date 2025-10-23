6190a1e9141ae0454d2787dcaf8772bcea9be061
Michael Garcia <130785882+mc727-garcia@users.noreply.github.com>
2025-10-23 16:42:39 -0700

    Deleted copies of src files and updated methods

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,13 +1,28 @@
 import java.util.Scanner; 
 public class Hotel implements HotelSystem{
     public void Login(){
-        Scanner input = new Scanner(System.in); 
+        Scanner Logininput = new Scanner(System.in); 
         System.out.println("Welcome, please log into your account: "); 
-        String username = input.nextLine(); 
-        String password = input.nextLine();
+        String username = Logininput.nextLine(); 
+        String password = Logininput.nextLine();
         // Must combine with Database to verify active account (.txt files) 
     }
     public void Payment(){
+        Scanner Paymentinput = new Scanner(System.in); 
+        String Credit; 
+        String Debit; 
+        System.out.println("Please enter your payment method: "); 
+        String Paymentmethod = Paymentinput.nextLine(); 
+        if (Paymentmethod.equals("Credit")){
+
+        }
+        else if (Paymentmethod.equals("Debit")){
+
+        }
+        else {
+            System.out.println("Invalid payment method, please contact Management.");
+        }
+
 
     }
     public void Reservation(){
