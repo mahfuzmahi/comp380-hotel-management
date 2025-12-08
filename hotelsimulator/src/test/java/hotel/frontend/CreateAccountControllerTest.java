5538de72d111c4d7e316230e0e341c31dab0f2cb
JustinScott2 <130512614+JustinScott2@users.noreply.github.com>
2025-12-07 21:54:01 -0800

    made a test

---
+++ hotelsimulator/src/test/java/hotel/frontend/CreateAccountControllerTest.java
@@ -0,0 +1,33 @@
+package hotel.frontend;
+import org.junit.jupiter.api.Test;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import hotel.backend.Hotel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CreateAccountControllerTest {
+    @Test
+    public void handleAdminButtonTest_False() {
+        //shows that account creation fails with null inputs
+        String tempNULL = null;
+        Hotel hotel = new Hotel();
+        assertFalse(hotel.createEmployeeAccount(tempNULL, tempNULL, tempNULL, tempNULL), "Account creation should fail with null inputs");
+    }
+
+    @Test
+    public void handleAdminButtonTest_True() {
+        //shows that account creation succeeds with valid inputs
+        boolean result;
+        try (FileWriter fw = new FileWriter(Hotel.filePath("employees.txt"), true)) {
+            result = true;
+        } catch (IOException e) {
+            System.out.println("Error writing to employees text file");
+            result = false;
+        }
+        assertTrue(result, "Account creation should succeed with valid inputs");
+    }
+
+
+}
